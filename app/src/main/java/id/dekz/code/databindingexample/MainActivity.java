745837e1f0312d88f4590f213a2f0da8105ff63c
Adi Andrea <dekz.itfz@gmail.com>
2016-06-29 11:25:49 +0800

    Inflate Layout in Data Binding's way

--- app/src/main/java/id/dekz/code/databindingexample/MainActivity.java
+++ app/src/main/java/id/dekz/code/databindingexample/MainActivity.java
@@ -1,13 +1,19 @@
 package id.dekz.code.databindingexample;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import id.dekz.code.databindingexample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.tvHello.setText("Bye findViewByID ~");
     }
 }
